2fd85788c115507e6c5616ff23ab477c80b99d78
congu <connerguillerman@gmail.com>
2026-06-24 13:52:12 -0500

    Finish code

--- createAppend.js
+++ createAppend.js
@@ -4,37 +4,39 @@ let container = document.querySelector(".container");
 
 // 1. Create an h2 element.
 // - Store it in a variable called h2.
-
+h2 = document.createElement("h2");
 
 // 2. Set the innerHTML of h2 to your name.
 
-
+h2.innerHTML = "Conner";
 
 // 3. Style at least two different properties for the heading.
 
-
+h2.style.backgroundColor = "black";
+h2.style.color = "white";
 
 // 4. Select the container div and append the h2 element.
 
-
+container.appendChild(h2);
 
 // 5. Create a paragraph element.
 // - Store it in a variable called para.
-
+let para = document.createElement("p");
 
 // 6. Set the innerHTML of para to a fact about yourself.
-
+para.innerHTML = "I have 2 dogs";
 
 // 7. Style at least two different properties for the paragraph.
-
+para.style.backgroundColor = "blue";
+para.style.color = "white";
 
 // 8. Select the container div and append the paragraph element.
-
+container.appendChild(para);
 
 
 // Uncomment the code below after completing 1-8.
 // Where do you see this on the page?
 // Why is it located there even though the body is selected?
-// let h1 = document.createElement('h1');
-// h1.innerHTML = "My Webpage";
-// body.appendChild(h1);
+let h1 = document.createElement('h1');
+h1.innerHTML = "My Webpage";
+body.appendChild(h1);
